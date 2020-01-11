d1bfa8e43ae4c92352b472e98928bada50b88b6e
SUMIT_THAKUR <sumitthakur769@gmail.com>
2020-01-12 02:38:46 +0530

    Remove swagger expose from rest-app

--- server/rest-app/rest-app.go
+++ server/rest-app/rest-app.go
@@ -7,9 +7,6 @@ import (
 	"gamezop/protos"
 	"log"
 	"net/http"
-	"path"
-
-	"strings"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
@@ -39,9 +36,6 @@ func ExposePoint(address string, opts ...runtime.ServeMuxOption) error {
 		return err
 	}
 	grpcMux := http.NewServeMux()
-	grpcMux.Handle("/", mux)
-	// serveSwagger(grpcMux)
-	grpcMux.HandleFunc("/swagger/", serveSwagger)
 	log.Println("Starting Endpoint Exposed Server: localhost:5051")
 	http.ListenAndServe(address, grpcMux)
 	return nil
@@ -56,11 +50,4 @@ func Init() {
 	if err := ExposePoint(":5051"); err != nil {
 		log.Fatal("Error in serve", err)
 	}
-}
-
-// serve swagger
-func serveSwagger(w http.ResponseWriter, r *http.Request) {
-	p := strings.TrimPrefix(r.URL.Path, "/swagger/")
-	p = path.Join("swagger/third_party/swagger-new/swagger-ui/", p)
-	http.ServeFile(w, r, p)
 }
